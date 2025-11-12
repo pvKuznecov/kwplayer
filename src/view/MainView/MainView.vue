441a39149245186441d8b1e6b7be6e54885a4136
Kuznetsov Pavel <p.v.kuznecov@yandex.ru>
2025-11-12 16:22:42 +0700

    alf

--- src/view/MainView/MainView.vue
+++ src/view/MainView/MainView.vue
@@ -1,12 +1,14 @@
 <template src="./MainView.html"></template>
 <style src="./MainView.css"></style>
 <script>
-    import MusicPlaylist from '@/components/MusicPlaylist/MusicPlaylist.vue';
+    // import MusicPlaylist from '@/components/MusicPlaylist/MusicPlaylist.vue';
+    import MusicPlayer from '@/components/MusicPlayer/MusicPlayer.vue';
 
     export default {
         name: 'MainView',
         components: {
-            MusicPlaylist
+            // MusicPlaylist
+            MusicPlayer
         },
     };
 </script>
